6c69d038d360e4b529e5752ffe13c85dbdb9ec7a
kanyanatsr67-hue <kanyanatsr67@nu.ac.th>
2026-06-30 19:54:51 +0700

    first commit

--- lab2_exercise3.dart
+++ lab2_exercise3.dart
@@ -1,12 +1,22 @@
-void main(){
+void main() {
   List<String> subjects = ['Math', 'Science', 'Biology'];
   print(subjects.length);
-  print('${subjects.first} , ${subjects.last}');
+  print(subjects[0] + ' ' + subjects[2]);
   subjects.add('Computer');
-  print(subjects.join(' '));
-  Map<String, int> studentScores = {'Math':72 , 'Computer':97};
+  print(subjects);
+  Map<String, int> studentScores = {'Math': 72, 'Computer': 49, 'Biology': 85};
   print(studentScores['Math']);
   studentScores['Science'] = 88;
-  //8 print(Map'$studentScores');
-  for (var i in studentScores)
+  print(studentScores);
+  for (var i in studentScores.keys) {
+    if (i.toLowerCase().contains('a')) {
+      print('$i : ${studentScores[i]}');
+    }
+  }
+  print('-------');
+  for (var j in studentScores.keys) {
+    if (studentScores[j]! >= 50) {
+      print('$j : ${studentScores[j]}');
+    }
+  }
 }
